3088450233f0c99712dfa9484bd248a6481e869c
Jonathan Steinbeck <jonathan@steinbeck.in>
2013-02-24 12:04:39 +0100

    Added a DOM typewriter effect to MO5. The effect works on HTML, too, not only on texts. The effect is located at MO5.dom.effects.typewriter.

--- js/libs/MO5.js
+++ js/libs/MO5.js
@@ -1829,6 +1829,96 @@ out.canvas.ImagePack.prototype.move = out.mixins.move;*/
 
 
     out.dom = {};
+    
+    out.dom.effects = {};
+    
+    out.dom.effects.typewriter = function (element, args)
+    {
+        var TYPE_ELEMENT = 1, TYPE_TEXT = 3, speed, cb;
+        
+        args = args || {};
+        speed = args.speed || 50;
+        cb = args.onFinish || null;
+        
+        function hideChildren(el)
+        {
+            var childNodes = el.childNodes, i, len;
+            
+            if (el.nodeType === TYPE_ELEMENT)
+            {
+                el.style.display = 'none';
+                
+                for (i = 0, len = childNodes.length; i < len; i += 1)
+                {
+                    hideChildren(childNodes[i]);
+                }
+            }
+        }
+        
+        hideChildren(element);
+        
+        function showChildren(el, cb)
+        {
+            if (el.nodeType === TYPE_ELEMENT)
+            {
+                (function ()
+                {
+                    var children = [], i, len;
+                    
+                    while (el.hasChildNodes())
+                    {
+                        children.push(el.removeChild(el.firstChild));
+                    }
+                    
+                    el.style.display = '';
+                    
+                    (function loopChildren()
+                    {
+                        if (children.length > 0)
+                        {
+                            showChildren(children[0], loopChildren);
+                            el.appendChild(children.shift());
+                        }
+                        else if (cb)
+                        {
+                            setTimeout(cb, 0);
+                        }
+                    }());
+                    
+                }());
+            }
+            else if (el.nodeType === TYPE_TEXT)
+            {
+                (function ()
+                {
+                    var textContent = el.data.replace(/ +/g, ' '), i, len;
+                    
+                    el.data = '';
+                    i = 0;
+                    len = textContent.length;
+                    
+                    function insertTextContent()
+                    {
+                        el.data += textContent[i];
+                        i += 1;
+                        
+                        if (i < len)
+                        {
+                            setTimeout(insertTextContent, 1000 / speed);
+                        }
+                        else if (cb)
+                        {
+                            setTimeout(cb, 0);
+                        }
+                    }
+                    
+                    insertTextContent();
+                }());
+            }
+        }
+        
+        showChildren(element, cb);
+    };
 
     out.dom.PrototypeObject = function ()
     {};
