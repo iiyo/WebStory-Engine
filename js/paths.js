5df2872737437bf8ba6bbea6bdcc9ff483947585
Jonathan Steinbeck <jonathan@steinbeck.in>
2015-05-30 19:05:48 +0200

    Changed paths.js to work from any directory.

--- js/paths.js
+++ js/paths.js
@@ -1,54 +1,66 @@
 /* global MO5 */
 
-MO5.modules["WSE.assets"] = "js/assets.js";
-MO5.modules["WSE.assets.Animation"] = "js/assets/Animation.js";
-MO5.modules["WSE.assets.Audio"] = "js/assets/Audio.js";
-MO5.modules["WSE.assets.Background"] = "js/assets/Background.js";
-MO5.modules["WSE.assets.Character"] = "js/assets/Character.js";
-MO5.modules["WSE.assets.Curtain"] = "js/assets/Curtain.js";
-MO5.modules["WSE.assets.Imagepack"] = "js/assets/Imagepack.js";
-MO5.modules["WSE.assets.Textbox"] = "js/assets/Textbox.js";
-MO5.modules["WSE.assets.mixins.displayable.flash"] = "js/assets/mixins/displayable/flash.js";
-MO5.modules["WSE.assets.mixins.displayable.flicker"] = "js/assets/mixins/displayable/flicker.js";
-MO5.modules["WSE.assets.mixins.displayable.hide"] = "js/assets/mixins/displayable/hide.js";
-MO5.modules["WSE.assets.mixins.displayable.move"] = "js/assets/mixins/displayable/move.js";
-MO5.modules["WSE.assets.mixins.displayable.shake"] = "js/assets/mixins/displayable/shake.js";
-MO5.modules["WSE.assets.mixins.displayable.show"] = "js/assets/mixins/displayable/show.js";
-
-MO5.modules["WSE.commands"] = "js/commands.js";
-MO5.modules["WSE.commands.alert"] = "js/commands/alert.js";
-MO5.modules["WSE.commands.break"] = "js/commands/break.js";
-MO5.modules["WSE.commands.choice"] = "js/commands/choice.js";
-MO5.modules["WSE.commands.confirm"] = "js/commands/confirm.js";
-MO5.modules["WSE.commands.do"] = "js/commands/do.js";
-MO5.modules["WSE.commands.fn"] = "js/commands/fn.js";
-MO5.modules["WSE.commands.global"] = "js/commands/global.js";
-MO5.modules["WSE.commands.globalize"] = "js/commands/globalize.js";
-MO5.modules["WSE.commands.goto"] = "js/commands/goto.js";
-MO5.modules["WSE.commands.line"] = "js/commands/line.js";
-MO5.modules["WSE.commands.localize"] = "js/commands/localize.js";
-MO5.modules["WSE.commands.prompt"] = "js/commands/prompt.js";
-MO5.modules["WSE.commands.restart"] = "js/commands/restart.js";
-MO5.modules["WSE.commands.set_vars"] = "js/commands/set_vars.js";
-MO5.modules["WSE.commands.sub"] = "js/commands/sub.js";
-MO5.modules["WSE.commands.trigger"] = "js/commands/trigger.js";
-MO5.modules["WSE.commands.var"] = "js/commands/var.js";
-MO5.modules["WSE.commands.wait"] = "js/commands/wait.js";
-MO5.modules["WSE.commands.while"] = "js/commands/while.js";
-MO5.modules["WSE.commands.with"] = "js/commands/with.js";
-
-MO5.modules["WSE.dataSources"] = "js/dataSources.js";
-MO5.modules["WSE.dataSources.LocalStorage"] = "js/dataSources/LocalStorage.js";
-
-MO5.modules["WSE.functions"] = "js/functions.js";
-
-MO5.modules["WSE.tools"] = "js/tools/tools.js";
-MO5.modules["WSE.tools.ui"] = "js/tools/ui.js";
-
-MO5.modules["WSE.Keys"] = "js/Keys.js";
-
-MO5.modules["WSE.DisplayObject"] = "js/DisplayObject.js";
-MO5.modules["WSE.Game"] = "js/Game.js";
-MO5.modules["WSE.Interpreter"] = "js/Interpreter.js";
-MO5.modules["WSE.Trigger"] = "js/Trigger.js";
-MO5.modules["WSE"] = "js/WSE.js";
+(function () {
+    
+    var base = MO5.path + "../../../";
+    
+    MO5.modules["WSE.assets"] = base + "js/assets.js";
+    MO5.modules["WSE.assets.Animation"] = base + "js/assets/Animation.js";
+    MO5.modules["WSE.assets.Audio"] = base + "js/assets/Audio.js";
+    MO5.modules["WSE.assets.Background"] = base + "js/assets/Background.js";
+    MO5.modules["WSE.assets.Character"] = base + "js/assets/Character.js";
+    MO5.modules["WSE.assets.Curtain"] = base + "js/assets/Curtain.js";
+    MO5.modules["WSE.assets.Imagepack"] = base + "js/assets/Imagepack.js";
+    MO5.modules["WSE.assets.Textbox"] = base + "js/assets/Textbox.js";
+    MO5.modules["WSE.assets.mixins.displayable.flash"] =
+        base + "js/assets/mixins/displayable/flash.js";
+    MO5.modules["WSE.assets.mixins.displayable.flicker"] =
+        base + "js/assets/mixins/displayable/flicker.js";
+    MO5.modules["WSE.assets.mixins.displayable.hide"] =
+        base + "js/assets/mixins/displayable/hide.js";
+    MO5.modules["WSE.assets.mixins.displayable.move"] = 
+        base + "js/assets/mixins/displayable/move.js";
+    MO5.modules["WSE.assets.mixins.displayable.shake"] =
+        base + "js/assets/mixins/displayable/shake.js";
+    MO5.modules["WSE.assets.mixins.displayable.show"] =
+        base + "js/assets/mixins/displayable/show.js";
+    
+    MO5.modules["WSE.commands"] = base + "js/commands.js";
+    MO5.modules["WSE.commands.alert"] = base + "js/commands/alert.js";
+    MO5.modules["WSE.commands.break"] = base + "js/commands/break.js";
+    MO5.modules["WSE.commands.choice"] = base + "js/commands/choice.js";
+    MO5.modules["WSE.commands.confirm"] = base + "js/commands/confirm.js";
+    MO5.modules["WSE.commands.do"] = base + "js/commands/do.js";
+    MO5.modules["WSE.commands.fn"] = base + "js/commands/fn.js";
+    MO5.modules["WSE.commands.global"] = base + "js/commands/global.js";
+    MO5.modules["WSE.commands.globalize"] = base + "js/commands/globalize.js";
+    MO5.modules["WSE.commands.goto"] = base + "js/commands/goto.js";
+    MO5.modules["WSE.commands.line"] = base + "js/commands/line.js";
+    MO5.modules["WSE.commands.localize"] = base + "js/commands/localize.js";
+    MO5.modules["WSE.commands.prompt"] = base + "js/commands/prompt.js";
+    MO5.modules["WSE.commands.restart"] = base + "js/commands/restart.js";
+    MO5.modules["WSE.commands.set_vars"] = base + "js/commands/set_vars.js";
+    MO5.modules["WSE.commands.sub"] = base + "js/commands/sub.js";
+    MO5.modules["WSE.commands.trigger"] = base + "js/commands/trigger.js";
+    MO5.modules["WSE.commands.var"] = base + "js/commands/var.js";
+    MO5.modules["WSE.commands.wait"] = base + "js/commands/wait.js";
+    MO5.modules["WSE.commands.while"] = base + "js/commands/while.js";
+    MO5.modules["WSE.commands.with"] = base + "js/commands/with.js";
+    
+    MO5.modules["WSE.dataSources"] = base + "js/dataSources.js";
+    MO5.modules["WSE.dataSources.LocalStorage"] = base + "js/dataSources/LocalStorage.js";
+    
+    MO5.modules["WSE.functions"] = base + "js/functions.js";
+    
+    MO5.modules["WSE.tools"] = base + "js/tools/tools.js";
+    MO5.modules["WSE.tools.ui"] = base + "js/tools/ui.js";
+    
+    MO5.modules["WSE.Keys"] = base + "js/Keys.js";
+    
+    MO5.modules["WSE.DisplayObject"] = base + "js/DisplayObject.js";
+    MO5.modules["WSE.Game"] = base + "js/Game.js";
+    MO5.modules["WSE.Interpreter"] = base + "js/Interpreter.js";
+    MO5.modules["WSE.Trigger"] = base + "js/Trigger.js";
+    MO5.modules["WSE"] = base + "js/WSE.js";
+    
+}());
