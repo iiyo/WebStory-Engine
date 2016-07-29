3f0030d105f79b1eb4ce54a8e708fa1f94d3d74e
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-07-30 00:10:33 +0200

    Renamed js/ folder to src/.

---
+++ src/paths.js
@@ -0,0 +1,59 @@
+/* global using */
+
+(function () {
+    
+    var base = using.path + "../../../../";
+    
+    using.modules["WSE.assets"] = base + "src/assets.js";
+    using.modules["WSE.assets.Animation"] = base + "src/assets/Animation.js";
+    using.modules["WSE.assets.Audio"] = base + "src/assets/Audio.js";
+    using.modules["WSE.assets.Background"] = base + "src/assets/Background.js";
+    using.modules["WSE.assets.Character"] = base + "src/assets/Character.js";
+    using.modules["WSE.assets.Curtain"] = base + "src/assets/Curtain.js";
+    using.modules["WSE.assets.Imagepack"] = base + "src/assets/Imagepack.js";
+    using.modules["WSE.assets.Textbox"] = base + "src/assets/Textbox.js";
+    using.modules["WSE.assets.Composite"] = base + "src/assets/Composite.js";
+    
+    using.modules["WSE.commands"] = base + "src/commands.js";
+    using.modules["WSE.commands.alert"] = base + "src/commands/alert.js";
+    using.modules["WSE.commands.break"] = base + "src/commands/break.js";
+    using.modules["WSE.commands.choice"] = base + "src/commands/choice.js";
+    using.modules["WSE.commands.confirm"] = base + "src/commands/confirm.js";
+    using.modules["WSE.commands.do"] = base + "src/commands/do.js";
+    using.modules["WSE.commands.fn"] = base + "src/commands/fn.js";
+    using.modules["WSE.commands.global"] = base + "src/commands/global.js";
+    using.modules["WSE.commands.globalize"] = base + "src/commands/globalize.js";
+    using.modules["WSE.commands.goto"] = base + "src/commands/goto.js";
+    using.modules["WSE.commands.line"] = base + "src/commands/line.js";
+    using.modules["WSE.commands.localize"] = base + "src/commands/localize.js";
+    using.modules["WSE.commands.prompt"] = base + "src/commands/prompt.js";
+    using.modules["WSE.commands.restart"] = base + "src/commands/restart.js";
+    using.modules["WSE.commands.set_vars"] = base + "src/commands/set_vars.js";
+    using.modules["WSE.commands.sub"] = base + "src/commands/sub.js";
+    using.modules["WSE.commands.trigger"] = base + "src/commands/trigger.js";
+    using.modules["WSE.commands.var"] = base + "src/commands/var.js";
+    using.modules["WSE.commands.wait"] = base + "src/commands/wait.js";
+    using.modules["WSE.commands.while"] = base + "src/commands/while.js";
+    using.modules["WSE.commands.with"] = base + "src/commands/with.js";
+    
+    using.modules["WSE.dataSources"] = base + "src/dataSources.js";
+    using.modules["WSE.dataSources.LocalStorage"] = base + "src/dataSources/LocalStorage.js";
+    
+    using.modules["WSE.functions"] = base + "src/functions.js";
+    
+    using.modules["WSE.tools"] = base + "src/tools/tools.js";
+    using.modules["WSE.tools.ui"] = base + "src/tools/ui.js";
+    using.modules["WSE.tools.reveal"] = base + "src/tools/reveal.js";
+    using.modules["WSE.tools.compile"] = base + "src/tools/compile.js";
+    
+    using.modules["WSE.Keys"] = base + "src/Keys.js";
+    
+    using.modules["WSE.loader"] = base + "src/loader.js";
+    using.modules["WSE.DisplayObject"] = base + "src/DisplayObject.js";
+    using.modules["WSE.Game"] = base + "src/Game.js";
+    using.modules["WSE.Interpreter"] = base + "src/Interpreter.js";
+    using.modules["WSE.LoadingScreen"] = base + "src/LoadingScreen.js";
+    using.modules["WSE.Trigger"] = base + "src/Trigger.js";
+    using.modules.WSE = base + "src/WSE.js";
+    
+}());
