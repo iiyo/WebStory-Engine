3b935040d38f4a099a7acc139d98389fdcf02bff
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-07-29 23:58:05 +0200

    Added a package.js scripts which creates a ZIP archive for the engine and puts it into packages/.

---
+++ package.js
@@ -0,0 +1,43 @@
+/* global require, console */
+
+var fs = require("fs");
+var normalizePath = require("path").normalize;
+var info = JSON.parse("" + fs.readFileSync("package.json"));
+var source = normalizePath("export/");
+var destination = normalizePath('packages/');
+var destinationFile = normalizePath(destination + "/WebStory-Engine-v" + info.version + ".zip");
+var archiver = require('archiver');
+var archive = archiver('zip');
+
+mkdirSync(destination);
+
+var output = fs.createWriteStream(destinationFile);
+
+archive.pipe(output);
+
+archive.bulk([{
+    src: [ '**/*' ],
+    cwd: source,
+    expand: true
+}]);
+
+archive.finalize(function(err, bytes) {
+    
+    if (err) {
+        throw err;
+    }
+    
+    console.log('Created ZIP file in packages/.');
+    
+});
+
+function mkdirSync (path) {
+    try {
+        fs.mkdirSync(path);
+    }
+    catch (e) {
+        if (e.code != 'EEXIST') {
+            throw e;
+        }
+    }
+}
