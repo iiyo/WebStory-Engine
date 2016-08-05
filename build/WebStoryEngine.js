c36665f7c1b4cdd31ac76efaae7d0f648992c9af
Jonathan Steinbeck <jonathan@steinbeck.in>
2016-08-06 00:16:36 +0200

    Increased version number to 2016.7.1.

--- build/WebStoryEngine.js
+++ build/WebStoryEngine.js
@@ -450,8 +450,8 @@ using.ajax = (function () {
 
 
 /*
-    WebStory Engine dependencies (v2016.7.0-final.1607301311)
-    Build time: Fri, 05 Aug 2016 21:54:13 GMT
+    WebStory Engine dependencies (v2016.7.1-final.1608060015)
+    Build time: Fri, 05 Aug 2016 22:16:15 GMT
 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* global using, require */
@@ -8601,7 +8601,7 @@ define("WSE", function (DataBus, assets, commands, dataSources, functions) {
     
     "use strict";
     
-    var WSE = {}, version = "2016.7.0-final.1607301311";
+    var WSE = {}, version = "2016.7.1-final.1608060015";
     
     DataBus.inject(WSE);
     
